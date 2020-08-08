e8f82f3f484c84dc7d116dc675e411fd40003014
Zero-A6M <artem3476@mail.ru>
2020-08-08 17:20:45 +0300

    update

---
+++ modules/user.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(name, pass, setting = {}) {
+        this.username = name;
+        this.money = setting.money || 1500;
+        this.isBanned = setting.isBanned || false;
+        this.isGuest = setting.isGuest || true;
+    }
+}
