dc6857a2bf54541bfd3c909bbad41c6c2465f39b
Zero-A6M <artem3476@mail.ru>
2020-08-12 19:59:57 +0300

    update

--- game/player.js
+++ game/player.js
@@ -7,3 +7,4 @@ class Player {
         this.isDefence = false;
     }
 }
+
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let flash = require('connect-flash');
 
 let router = require('express').Router();
 
-let dbClient = new mongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+//let dbClient = new mongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
 let app = new express();
 
 function checkAuth() {
@@ -35,52 +35,68 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static("static"));
 
-passport.use(new localStrategy((user, password, done) => {
-    if(user !== 'test_user')
-        return done(null, false, {message: 'User not found'});
-    else if(password !== '16ec1ebb01fe02ded9b7d5447d3dfc65')
-    return done(null, false, {message: 'Wrong password'});
+passport.use(new localStrategy(async(user, password, done) => {
+    let dbClient = new mongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
+    let myPromise = () => {
+        return new Promise((resolve, reject) => {
+            dbClient.connect(async(err, client) => {
+                if (err) return console.error(err);
+    
+                const db = client.db("local");
+    
+                var myPromise2 = () => {
+                    return new Promise((resolve, reject) => {
+                
+                    db
+                        .collection('db_users')
+                        .find({username: user.toUpperCase()})
+                        .limit(1)
+                        .toArray(function(err, data) {
+                            err 
+                            ? reject(err) 
+                            : resolve(data[0]);
+                        });
+                    });
+                };
+                let _us = await myPromise2();
 
-    return done(null, {id: 1, name: 'Test', age: 21});
+                _us ? resolve(_us): reject({message: {type: "error", code: "001", message: "User not found"}});
+            });
+        });
+    };
+
+    let currentUser = await myPromise().catch((err) => {
+        console.error(err.message);
+        return null;
+    });
+
+    if(currentUser) {
+        if (password === currentUser.password) {
+            return done(null, currentUser.DataUser);
+        }
+        return done(null, false, {message: 'Wrong password'});
+    }
+
+    return done(null, false, {message: 'User not found'});
 }));
 
 app.post("/logout", function(req, res) {
     res.redirect('/');
 });
 
-app.post("/login", passport.authenticate('local', {session: false, successRedirect: '/page', failureRedirect: '/', failureFlash: 'Invalid username or password.', successFlash: 'Welcome!'}), function(req, res) {
-
+app.post("/login", passport.authenticate('local', {session: true, failureFlash: 'Invalid username or password.', successFlash: 'Welcome!'}), function(req, res) {
+    res.json(req.user);
 });
 
 app.post("/checkin", (req, res) => {
     console.log(req.body);
-    dbClient = new mongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+    let dbClient = new mongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
     dbClient.connect((err, client) => {
         if (err) return console.error(err);
 
         const db = client.db("local");
         const collection = db.collection("db_users");
-        let user = {username: req.body.username, password: req.body.password, DataUser: new _user(req.body.username, {isGuest: true, money: 1500})};
-        collection.insertOne(user, function(err, result){
-            if(err){ 
-                return console.log(err);
-            }
-            console.log(result.ops);
-            client.close();
-            res.json({message: 'User added', status: true, user: result.ops});
-        });
-    });
-});
-
-app.post("/db", (req, res) => {
-    console.log(req.body);
-    dbClient = new mongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
-    dbClient.connect((err, client) => {
-        if (err) return console.error(err);
-
-        const db = client.db("local");
-        const collection = db.collection("db_users");
-        let user = {username: req.body.username, password: req.body.password, DataUser: new _user(req.body.username, {isGuest: true, money: 1500})};
+        let user = {username: req.body.username.toUpperCase(), password: req.body.password, DataUser: new _user(req.body.username, {isGuest: true, money: 1500})};
         collection.insertOne(user, function(err, result){
             if(err){ 
                 return console.log(err);
