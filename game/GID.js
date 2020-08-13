0b9a921ea8f27988874a9c834bf0f53b72af78ee
Zero-A6M <artem3476@mail.ru>
2020-08-13 20:48:54 +0300

    add new file GID.js and update

---
+++ game/GID.js
@@ -0,0 +1,108 @@
+class GID {
+    constructor() {}
+
+    static #_g = this.gen();
+
+    static *gen() {
+        yield 'init';
+        yield 0x11;
+        yield 0x22;
+        yield 0x33;
+        yield 0x44;
+        yield 0x55;
+        yield 0x66;
+        yield 0x77;
+        yield 0x88;
+        yield 0x99;
+        yield 0xaa;
+    }
+
+    static GetHex(isNew = false) {
+        if (isNew) this.#_g = this.gen();
+        let _ = this.#_g.next();
+        if (_.done) {
+            this.#_g = this.gen();
+            this.#_g.next();
+            _ = this.#_g.next();
+        }
+
+        return _.value;
+    }
+
+    static GetBytes(str) {
+        let bytes1 = [];
+        let bytes2 = [];
+        this.GetHex(true);
+
+        for (let i = 0; i < str.length; i+=2)
+            bytes1.push(str[i].charCodeAt());
+
+        for (let i = 1; i < str.length; i+=2)
+            bytes2.push(str[i].charCodeAt());
+        
+        if (bytes1.length < 4) {
+            for (let i = 0; bytes1.length != 4; i++)
+                bytes1.push(bytes2[bytes2.length - i - 1] ^ this.GetHex());
+        }
+        
+        if (bytes2.length < 4) {
+            for (let i = 0; bytes2.length != 4; i++)
+                bytes2.push(bytes1[i] ^ this.GetHex());
+        }
+        
+        if (bytes1.length !== bytes2.length) bytes2.push(bytes1[0] ^ bytes2[bytes2.length - 1]);
+
+        return {bytes1, bytes2};
+    }
+
+    static Mixer(str) {
+        let {bytes1, bytes2} = this.GetBytes(str);
+        let bytes = [];
+        
+        for (let i = 0; bytes1.length !== 16; i++) {
+            bytes1.push(bytes2[bytes2.length - i - 1] ^ bytes1[i] ^ this.GetHex());
+        }
+        
+        for (let i = 0; bytes2.length !== 16; i++) {
+            bytes2.push(bytes1[bytes1.length - i - 1] ^ bytes2[i] ^ this.GetHex());
+        }
+        
+        bytes.push(bytes1[0] ^ bytes2[3] ^ this.GetHex());
+        bytes.push(bytes1[3] ^ bytes2[12] ^ this.GetHex());
+        bytes.push(bytes1[2] ^ bytes2[7] ^ this.GetHex());
+        bytes.push(bytes1[7] ^ bytes2[5] ^ this.GetHex());
+        
+        bytes.push(bytes1[9] ^ bytes2[14] ^ this.GetHex());
+        bytes.push(bytes1[10] ^ bytes2[1] ^ this.GetHex());
+        bytes.push(bytes1[8] ^ bytes2[15] ^ this.GetHex());
+        bytes.push(bytes1[4] ^ bytes2[6] ^ this.GetHex());
+        
+        bytes[0] ^= (bytes1[6] ^ bytes2[0]);
+        bytes[1] ^= (bytes1[1] ^ bytes2[13]);
+        bytes[2] ^= (bytes1[15] ^ bytes2[9]);
+        bytes[3] ^= (bytes1[5] ^ bytes2[11]);
+        
+        bytes[4] ^= (bytes1[14] ^ bytes2[2]);
+        bytes[5] ^= (bytes1[13] ^ bytes2[10]);
+        bytes[6] ^= (bytes1[12] ^ bytes2[4]);
+        bytes[7] ^= (bytes1[11] ^ bytes2[8]);
+        
+        return bytes;
+    }
+
+    static GetGID(str) {
+        let alphabet = "ABCDEFGHIJKabcdefghijk0123456789";
+
+        let result = "";
+        let bytes = this.Mixer(str);
+        
+        for (let i = 0; i < 8; i++) {
+            result += alphabet[bytes[i] >> 3];
+            result += alphabet[bytes[i] & 0x1F];
+        }
+
+        return result;
+    }
+}
+
+module.exports.GID = GID;
--- game/user.js
+++ game/user.js
@@ -1,4 +1,5 @@
 let random = require('./random');
+let gid = require('./GID').GID;
 
 class User {
     constructor(name, setting = {}) {
@@ -9,50 +10,8 @@ class User {
         this.isPremium = setting.isPremium || false;
         this.bannedTime = 0;
         this.premiumTime = 0;
-        this.GID = User.GenerateGID(this.username);
-    }
-
-    static GetNum(name) {
-        let bytes = [];
-        let num1 = -1;
-        let num2 = -1;
-
-        for (let i = 0; i < 4; i++) {
-            let _ = name[i].charCodeAt() & 255;
-            bytes.push(_)
-        }
-
-        bytes.push(bytes[0] ^ bytes[2]);
-        bytes.push(bytes[1] ^ bytes[3]);
-        bytes.push(name[name.length - 2].charCodeAt() & 255 ^ bytes[bytes.length - 1]);
-        bytes.push(name[name.length - 1].charCodeAt() & 255 ^ bytes[bytes.length - 1]);
-
-        num1 = bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
-        num2 = bytes[4] << 24 | bytes[5] << 16 | bytes[6] << 8 | bytes[7];
-
-        return num1 ^ num2;
-    }
-
-    static GenerateGID(name) {
-        let alphabet = "ABCDEFGHIJKabcdefghijk0123456789";
-
-        let result = "";
-        let _r = new random.Random(this.GetNum(name))
-        let bytes = [];
-
-        _r.NextBytes(bytes, 6);
-
-        for (let i =0; i < 6; i++) {
-            result += alphabet[bytes[i] >> 3];
-            result += alphabet[bytes[i] & 0x1F];
-        }
-
-        return result;
+        this.GID = gid.GetGID(name);
     }
 }
-
-
-
-
 
 module.exports.User = User;
